b8f7da98446b5c457b7a04cfdd60e58a5bc7f64f
finnif <wangshuang@unity3d.com>
2019-01-29 09:50:14 +0800

    fix AppState

--- Assets/ConnectApp/models/AppState.cs
+++ Assets/ConnectApp/models/AppState.cs
@@ -1,130 +1,143 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using NUnit.Framework;
 
 namespace ConnectApp.models {
   public class AppState {
-    private readonly Dictionary<string, dynamic> _state;
+    private readonly Dictionary<string, object> _state;
 
-    AppState() {
-      _state = new Dictionary<string, dynamic>();
+    private AppState() {
+      _state = new Dictionary<string, object>();
     }
 
-    AppState fromJson(dynamic json) => _dictToState(json);
+    AppState FromJson(Dictionary<string, object> json) => _dictToState(json);
 
-    public void set(string key, dynamic value) {
+    Dictionary<string, object> ToJson() => _stateToDict(this);
+
+    public void Set(string key, object value) {
       if (!_checkType(value)) {
         throw new ApplicationException($"Cannot set state {key} using value cannot be serialized.");
       }
 
-      dynamic fVal;
-      if (value is List<dynamic>) {
-        fVal = _listToState(value);
-      }
-      else if (value is Dictionary<string, dynamic>) {
-        fVal = _dictToState(value);
-      }
-      else {
-        fVal = value;
+      object fVal;
+      switch (value) {
+        case List<object> objects:
+          fVal = _listToState(objects);
+          break;
+        case Dictionary<string, object> dictionary:
+          fVal = _dictToState(dictionary);
+          break;
+        default:
+          fVal = value;
+          break;
       }
 
       this._set(key.Split('.'), fVal);
     }
 
-    private void _set(IReadOnlyList<string> keys, dynamic value) {
+    private void _set(IReadOnlyList<string> keys, object value) {
       if (keys.Count > 1) {
-        if (_state[keys[0]] && _state[keys[0]].GetType() is AppState) {
-          (_state[keys[0]] as AppState)?._set(keys.Skip(1).ToArray(), value);
+        if (_state[keys[0]] != null && _state[keys[0]] is AppState appState) {
+          appState._set(keys.Skip(1).ToArray(), value);
         }
         else {
-          var appState = new AppState();
-          var subState = appState._set(keys.Skip(1).ToArray(), value);
-          _state[keys[0]] = subState;
+          var newState = new AppState();
+          newState._set(keys.Skip(1).ToArray(), value);
+          _state[keys[0]] = newState;
         }
       }
       else {
         _state[keys[0]] = value;
       }
     }
-    
-    public dynamic get(string key, dynamic defaultValue) {
+
+    public object Get(string key, object defaultValue) {
       return _get(key.Split('.')) ?? defaultValue;
     }
 
-    private dynamic _get(IReadOnlyList<string> keys) {
+    private object _get(IReadOnlyList<string> keys) {
       if (!_state.ContainsKey(keys[0])) return null;
       var val = _state[keys[0]];
-      if (val.GetType() is AppState) {
-        if (keys.Count == 1) {
-          return _stateToDict(val);
-        }
-        return (val as AppState)?._get(keys.Skip(1).ToArray());
-      } else if (val is List<dynamic>) {
-        return _stateToList(val);
+      switch (val) {
+        case AppState appState:
+          return keys.Count == 1 ? _stateToDict(appState) : appState._get(keys.Skip(1).ToArray());
+        case List<object> objects:
+          return _stateToList(objects);
+        default:
+          return val;
       }
-      return val;
     }
 
     AppState clone() {
       return _cloneState(this);
     }
 
-    Dictionary<string, dynamic> toJson() => _stateToDict(this);
-
-    private static List<dynamic> _cloneList(IEnumerable<dynamic> list) {
-      var cloned = new List<dynamic>();
+    private static List<object> _cloneList(IEnumerable<object> list) {
+      var cloned = new List<object>();
       foreach (var item in list) {
-        if (item.GetType() is List<dynamic>) {
-          cloned.Add(_cloneList(item));
-        } else if (item.GetType() is AppState) {
-          cloned.Add(_cloneState(item));
-        }
-        else {
-          cloned.Add(item);
+        switch (item) {
+          case List<object> objects:
+            cloned.Add(_cloneList(objects));
+            break;
+          case AppState appState:
+            cloned.Add(_cloneState(appState));
+            break;
+          default:
+            cloned.Add(item);
+            break;
         }
       }
+
       return cloned;
     }
 
     private static AppState _cloneState(AppState state) {
       var cloned = new AppState();
       foreach (var key in state._state.Keys) {
-        if (state._state[key] is List) {
-          cloned._set(new []{key}, _cloneList(state._state[key]));
-        } else if (state._state[key].GetType() is AppState) {
-          cloned._set(new []{key}, _cloneState(state._state[key]));
-        }
-        else {
-          cloned._set(new []{key}, state._state[key]);
+        switch (state._state[key]) {
+          case List<object> objects:
+            cloned._set(new[] {key}, _cloneList(objects));
+            break;
+          case AppState appState:
+            cloned._set(new[] {key}, _cloneState(appState));
+            break;
+          default:
+            cloned._set(new[] {key}, state._state[key]);
+            break;
         }
       }
+
       return cloned;
     }
-    
-    private static  Dictionary<string, dynamic> _stateToDict(AppState state) {
-      var dict = new Dictionary<string, dynamic>();
+
+    private static Dictionary<string, object> _stateToDict(AppState state) {
+      var dict = new Dictionary<string, object>();
       foreach (var key in state._state.Keys) {
-        if (state._state[key].GetType() is AppState) {
-          dict[key] = _stateToDict(state._state[key]);
-        } else if (state._state[key].GetType() is List<dynamic>) {
-          dict[key] = _stateToList(state._state[key]);
-        } else {
-          dict[key] = state._state[key];
+        switch (state._state[key]) {
+          case AppState appState:
+            dict[key] = _stateToDict(appState);
+            break;
+          case List<object> objects:
+            dict[key] = _stateToList(objects);
+            break;
+          default:
+            dict[key] = state._state[key];
+            break;
         }
       }
+
       return dict;
     }
-    
-    private static AppState _dictToState(Dictionary<string, dynamic> dict) {
+
+    private static AppState _dictToState(Dictionary<string, object> dict) {
       var state = new AppState();
       foreach (var key in dict.Keys) {
         switch (dict[key]) {
-          case Dictionary<string, dynamic> _:
-            state._state[key] = _dictToState(dict[key]);
+          case Dictionary<string, object> _:
+            state._state[key] = _dictToState((Dictionary<string, object>) dict[key]);
             break;
-          case List<dynamic> _:
-            state._state[key] = _listToState(dict[key]);
+          case List<object> _:
+            state._state[key] = _listToState((List<object>) dict[key]);
             break;
           default:
             state._state[key] = dict[key];
@@ -135,31 +148,34 @@ namespace ConnectApp.models {
       return state;
     }
 
-    private static List<dynamic> _stateToList(List<dynamic> state) {
-      var list = new List<dynamic>();
+    private static List<object> _stateToList(IEnumerable<object> state) {
+      var list = new List<object>();
       foreach (var item in state) {
-        if (item is List<dynamic>) {
-          list.Add(_stateToList(item));
-        }
-        else if (item.GetType() is AppState) {
-          list.Add(_stateToDict(item));
-        }
-        else {
-          list.Add(item);
+        switch (item) {
+          case List<object> objects:
+            list.Add(_stateToList(objects));
+            break;
+          case AppState appState:
+            list.Add(_stateToDict(appState));
+            break;
+          default:
+            list.Add(item);
+            break;
         }
       }
+
       return list;
     }
-    
-    private static List<dynamic> _listToState(IEnumerable<dynamic> list) {
-      var state = new List<dynamic>();
+
+    private static List<object> _listToState(IEnumerable<object> list) {
+      var state = new List<object>();
       foreach (var item in list) {
         switch (item) {
-          case List<dynamic> _:
-            state.Add(_listToState(item));
+          case List<object> objects:
+            state.Add(_listToState(objects));
             break;
-          case Dictionary<string, dynamic> _:
-            state.Add(_dictToState(item));
+          case Dictionary<string, object> dictionary:
+            state.Add(_dictToState(dictionary));
             break;
           default:
             state.Add(item);
@@ -170,57 +186,64 @@ namespace ConnectApp.models {
       return state;
     }
 
-    private static bool _checkType(dynamic value) {
-       var type = value.GetType();
-      if (!(type is int) && 
-          !(type is double) && 
-          !(type is bool) && 
-          !(type is string) && 
-          !(type is null) && 
-          !(value is List) && 
-          !(value is Dictionary<string, dynamic>)) {
+    private static bool _checkType(object value) {
+      var type = value;
+      if (!(type is int) &&
+          !(type is double) &&
+          !(type is bool) &&
+          !(type is string) &&
+          !(type is null) &&
+          !(value is List<object>) &&
+          !(value is Dictionary<string, object>)) {
         return false;
       }
 
-      if (value is Dictionary<string, dynamic>) {
-        foreach (var key in value.keys) {
-          if (!(key.GetType() is string)) {
+      if (value is Dictionary<string, object> dictionary) {
+        foreach (var key in dictionary.Keys) {
+          if (key == null) {
             return false;
           }
-          var valid = _checkType(value[key]);
-          if (!valid) {
-            return false;
-          }
-        }
-      } else if (value is List) {
-        foreach (var item in value) {
-          var valid = _checkType(item);
+
+          var valid = _checkType(dictionary[key]);
           if (!valid) {
             return false;
           }
         }
       }
-
+      else {
+        var objects = (List<object>) value;
+        if (objects != null) {
+          foreach (var item in objects) {
+            var valid = _checkType(item);
+            if (!valid) {
+              return false;
+            }
+          }
+        }
+      }
       return true;
     }
 
     AppState emptyState() => new AppState();
 
-    AppState initialState() {
-      var state = _dictToState(
-        new Dictionary<string, dynamic> {
-          {"login", new Dictionary<string, dynamic> {
-            {"email", ""},
-            {"loading", false},
-            {"isLoggedIn", false}
-          }},
-          {"settings", new Dictionary<string, dynamic> {
-            {"language", "zh_CN"}
-          }},
-          {"users", new Dictionary<string, dynamic>()},
-          {"cookies", new List<string>()}
-      });
-      return state;
+      AppState initialState() {
+        var state = _dictToState(
+          new Dictionary<string, object> {
+            {
+              "login", new Dictionary<string, object> {
+                {"email", ""},
+                {"loading", false},
+                {"isLoggedIn", false}
+              }
+            }, {
+              "settings", new Dictionary<string, object> {
+                {"language", "zh_CN"}
+              }
+            },
+            {"users", new Dictionary<string, object>()},
+            {"cookies", new List<string>()}
+          });
+        return state;
+      }
     }
-  }
 }
