3e753b5d7d6759e69854a7f6a3077e84879b7cda
luoxuguang <xuguang.luo@unity3d.com>
2019-02-26 19:04:27 +0800

    custom_tab_bar

--- Assets/ConnectApp/components/custom_tab_bar.cs
+++ Assets/ConnectApp/components/custom_tab_bar.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections.Generic;
 using ConnectApp.constants;
-using NUnit.Framework;
+using Unity.UIWidgets.animation;
 using Unity.UIWidgets.foundation;
+using Unity.UIWidgets.material;
 using Unity.UIWidgets.painting;
 using Unity.UIWidgets.rendering;
+using Unity.UIWidgets.ui;
 using Unity.UIWidgets.widgets;
+using Image = Unity.UIWidgets.widgets.Image;
+using TextStyle = Unity.UIWidgets.painting.TextStyle;
 
 namespace ConnectApp.components
 {
@@ -18,6 +23,7 @@ namespace ConnectApp.components
         {
             this.controllers = controllers;
             this.items = items;
+            
         }
 
         public readonly List<Widget> controllers;
@@ -33,7 +39,9 @@ namespace ConnectApp.components
     {
         private PageController _pageController;
         private int _selectedIndex;
+        
         private const int kTabBarHeight = 50;
+        private Widget _body;
         
         public override Widget build(BuildContext context)
         {
@@ -47,19 +55,17 @@ namespace ConnectApp.components
             );
         }
 
-        private Widget _contentView() {
-            return new Flexible(
-                child: new Container(
-                    color: CColors.blue,
-                    decoration: new BoxDecoration(CColors.background1),
-                    child: new PageView(
-                        physics: new BouncingScrollPhysics(),
-                        controller: _pageController,
-                        onPageChanged: (int index) => { setState(() => { _selectedIndex = index; }); },
-                        children: widget.controllers
+        private Widget _contentView()
+        {
+           return new Container(
+                    child: new Container(
+                        height:MediaQuery.of(context).size.height-kTabBarHeight,
+                        color: CColors.blue,
+                        decoration: new BoxDecoration(CColors.background1),
+                        child: widget.controllers[_selectedIndex]
                     )
-                )
-            );
+                );
+
         }
 
         private Widget _bottomTabBar()
@@ -67,9 +73,9 @@ namespace ConnectApp.components
             return new Flexible(
                 child: new Container(
                     height:kTabBarHeight,
-                    color: CColors.red,
+                    color: Color.black,
                     child:new Row(
-                        mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                        mainAxisAlignment:MainAxisAlignment.spaceAround,
                         children: _buildItems()
                     )
                 )
@@ -77,21 +83,52 @@ namespace ConnectApp.components
         }
 
         List<Widget> _buildItems()
-        {
+        {         
             List<Widget> _widgets = new List<Widget>();
             widget.items.ForEach(item =>
             {
-                _widgets.Add(
-                    new Flexible(
-                        child:new Column(
-                            children:new List<Widget>
-                            {
-                                Image.asset(item.activeImge),
-                                new Text(item.title,style:new TextStyle(fontSize:14))
-                            }
-                        )
+                Widget _bulidItem = new Flexible(
+                    child: new Stack(
+                        fit: StackFit.expand,
+                        children: new List<Widget>
+                        {
+                            new GestureDetector(
+                                onTap: () =>
+                                {
+                                    if (_selectedIndex != item.index) setState(() => _selectedIndex = item.index);
+
+                                },
+                                child: new Container(
+                                    decoration: new BoxDecoration(
+                                        color: Color.clear),
+                                    child: new Column(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: new List<Widget>
+                                        {
+                                            new Padding(
+                                                padding: EdgeInsets.only(top: 5),
+                                                child: Image.asset(
+                                                    _selectedIndex == item.index ? item.activeImge : item.inactiveImge,
+                                                    height: 24, width: 24)
+                                            ),
+                                            new Padding(
+                                                padding: EdgeInsets.only(top: 5),
+                                                child: new Text(item.title,
+                                                    style: new TextStyle(fontSize: 9,
+                                                        color: _selectedIndex == item.index
+                                                            ? Colors.blue
+                                                            : Color.white))
+                                            ),
+
+                                        }
+                                    )
+                                )
+                            )
+                        }
                     )
+
                 );
+                _widgets.Add(_bulidItem);
             });
             
             return _widgets;
