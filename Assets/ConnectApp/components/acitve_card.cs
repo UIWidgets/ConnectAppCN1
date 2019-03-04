8538d198656dc41cc4f3ea2a81dd7e050f38fab2
luoxuguang <xuguang.luo@unity3d.com>
2019-03-04 17:38:39 +0800

    active_card add model

--- Assets/ConnectApp/components/acitve_card.cs
+++ Assets/ConnectApp/components/acitve_card.cs
@@ -1,36 +1,39 @@
+using System;
 using System.Collections.Generic;
 using ConnectApp.constants;
+using ConnectApp.models;
 using Unity.UIWidgets.foundation;
 using Unity.UIWidgets.painting;
 using Unity.UIWidgets.rendering;
+using Unity.UIWidgets.ui;
 using Unity.UIWidgets.widgets;
+using Image = Unity.UIWidgets.widgets.Image;
+using TextStyle = Unity.UIWidgets.painting.TextStyle;
 
 namespace ConnectApp.components
 {
-    public class AcitveCard : StatefulWidget
+    public class AcitveCard : StatelessWidget
     {
         public AcitveCard(
             
+            IEvent model,
             Key key =null
             
         ) : base(key)
         {
+            this.model = model;
         }
 
-        public override State createState()
-        {
-            return new _AcitveCardState();
-        }
-    }
-
-    internal class _AcitveCardState : State<AcitveCard>
-    {
+        public readonly IEvent model;
+        
         public override Widget build(BuildContext context)
         {
+            DateTime time = Convert.ToDateTime(model.createdTime);
             return new Container(
                 height:108,
                 padding:EdgeInsets.only(top:16,bottom:16,right:16),
                 child:new Row(
+//                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children:new List<Widget>
                     {
                         //date
@@ -40,40 +43,64 @@ namespace ConnectApp.components
                                 crossAxisAlignment:CrossAxisAlignment.center,
                                 children:new List<Widget>
                                 {
-                                    new Text("27",style:new TextStyle(height: 1.33f,
+                                    new Text(time.Day.ToString(),style:new TextStyle(height: 1.33f,
                                         fontSize: 24,
                                         fontFamily: "DINPro-Bold",
                                         color: CColors.secondaryPink)),
-                                    new Text("2月",style:CTextStyle.PSmall)
+                                    new Text($"{time.Month.ToString()}月",style:CTextStyle.PSmall)
                                 }    
                             )    
                         ),
                     
                         //content
                         new Container(
-                            width:MediaQuery.of(context).size.width-188,
+                            width:MediaQuery.of(context).size.width-196,
                             margin:EdgeInsets.only(right:8),
                             child:new Column(
-                                crossAxisAlignment:CrossAxisAlignment.start,
                                 mainAxisAlignment:MainAxisAlignment.spaceBetween,
+                                crossAxisAlignment:CrossAxisAlignment.start,
                                 children:new List<Widget>
                                 {
                                     new Container(
-                                        child:new Text("Unity2018.3全新Prefab预制件系统深入介绍",style:CTextStyle.PLarge,maxLines:2)
+                                        child:new Text(model.title,style:CTextStyle.PLarge,maxLines:2)
                                     ),
                                     
-                                    new Text("20:00 · 48人已预订",style:CTextStyle.PSmall)
+                                    new Text(model.live?$"20:00 · {model.participantsCount}人已预订":"20:00 · 旧金山Unity大厦",style:CTextStyle.PSmall)
                                 }  
                              )
                             ),
                         //pic
-                        new Container(
-                            child:new ClipRRect(
-                                borderRadius:BorderRadius.all(4),
-                                child:new Container(
-                                    width:114,
-                                    child:Image.asset("pikachu",fit:BoxFit.cover)    
-                                    
+                        new ClipRRect(
+                            borderRadius:BorderRadius.all(4),
+                            child: new Container(
+                                width: 114,
+                                height: 76,
+                                child: new Stack(
+//                            fit: StackFit.expand,
+                                    children:new List<Widget>
+                                    {
+                                        new Container(
+//                                            width:114,
+//                                            height:76,
+                                            child:Image.network(model.background,fit:BoxFit.cover)    
+                                        )
+//                                new Positioned(
+//                                    bottom:0,
+//                                    right:0,
+//                                    child:new Container(
+//                                        width:41,
+//                                        height:24,
+//                                        color:model.live?CColors.PrimaryBlue:CColors.secondaryPink,
+//                                        child:new Text(model.live?"线上":"线下",style:new TextStyle(
+//                                            height: 1.67f,
+//                                            fontSize: 12,
+//                                            fontFamily: "PingFang-Regular",
+//                                            color: CColors.White
+//                                        ),textAlign:TextAlign.center)
+//                                        
+//                                    )
+//                                )
+                                    } 
                                 )
                             )
                         )
