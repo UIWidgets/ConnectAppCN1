607f34da896d1c5f0ee40e12513ae4b2f673f8f8
luoxuguang <xuguang.luo@unity3d.com>
2019-04-01 16:01:47 +0800

    config

---
+++ Assets/ConnectApp/constants/Config.cs
@@ -0,0 +1,8 @@
+namespace ConnectApp.constants {
+    public static class Config {
+        public const string apiAddress = "https://connect.unity.com";
+        
+        public const string wechatAppId = "wx0ab79f0c7db7ca52";
+
+    }
+}
