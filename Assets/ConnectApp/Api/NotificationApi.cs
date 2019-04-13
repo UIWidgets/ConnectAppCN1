cea510b47f63274281da7e43d9d3c955f7a09661
luoxuguang <xuguang.luo@unity3d.com>
2019-04-13 23:40:47 +0800

    httpmanager resume

--- Assets/ConnectApp/Api/NotificationApi.cs
+++ Assets/ConnectApp/Api/NotificationApi.cs
@@ -11,66 +11,30 @@ using Unity.UIWidgets.ui;
 namespace ConnectApp.api {
     public static class NotificationApi {
         public static Promise<FetchNotificationResponse> FetchNotifications(int pageNumber) {
-            // We return a promise instantly and start the coroutine to do the real work
             var promise = new Promise<FetchNotificationResponse>();
-            Window.instance.startCoroutine(_FetchNotifications(promise, pageNumber));
-            return promise;
-        }
-
-        private static IEnumerator _FetchNotifications(Promise<FetchNotificationResponse> promise, int pageNumber) {
             var request = HttpManager.GET(Config.apiAddress + "/api/notifications?page=" + pageNumber);
-            yield return request.SendWebRequest();
-            if (request.isNetworkError) {
-                // something went wrong
-                promise.Reject(new Exception(request.error));
-            }
-            else if (request.responseCode != 200) {
-                // or the response is not OK
-                promise.Reject(new Exception(request.downloadHandler.text));
-            }
-            else {
-                if (request.GetResponseHeaders().ContainsKey("SET-COOKIE")) {
-                    var cookie = request.GetResponseHeaders()["SET-COOKIE"];
-                    HttpManager.updateCookie(cookie);
-                }
-
-                // Format output and resolve promise
-                var responseText = request.downloadHandler.text;
+            HttpManager.resume(request).Then(responseText =>
+            {  
                 var notificationResponse = JsonConvert.DeserializeObject<FetchNotificationResponse>(responseText);
-                if (notificationResponse != null)
-                    promise.Resolve(notificationResponse);
-                else
-                    promise.Reject(new Exception("No user under this username found!"));
-            }
+                promise.Resolve(notificationResponse);
+            }).Catch(exception =>
+            {
+                promise.Reject(exception);  
+            });
+            return promise;
         }
         
         public static Promise FetchMakeAllSeen() {
-            // We return a promise instantly and start the coroutine to do the real work
             var promise = new Promise();
-            Window.instance.startCoroutine(_FetchMakeAllSeen(promise));
-            return promise;
-        }
-
-        private static IEnumerator _FetchMakeAllSeen(Promise promise) {
-            var request = HttpManager.initRequest(Config.apiAddress + "/api/notifications/make-all-seen", "POST");
-            yield return request.SendWebRequest();
-            if (request.isNetworkError) {
-                // something went wrong
-                promise.Reject(new Exception(request.error));
-            }
-            else if (request.responseCode != 200) {
-                // or the response is not OK
-                promise.Reject(new Exception(request.downloadHandler.text));
-            }
-            else {
-                if (request.GetResponseHeaders().ContainsKey("SET-COOKIE")) {
-                    var cookie = request.GetResponseHeaders()["SET-COOKIE"];
-                    HttpManager.updateCookie(cookie);
-                }
-                
-                // Format output and resolve promise
+            var request = HttpManager.initRequest(Config.apiAddress + "/api/notifications/make-all-seen", Method.POST);
+            HttpManager.resume(request).Then(responseText =>
+            {  
                 promise.Resolve();
-            }
+            }).Catch(exception =>
+            {
+                promise.Reject(exception);  
+            });
+            return promise;
         }
     }
 }
