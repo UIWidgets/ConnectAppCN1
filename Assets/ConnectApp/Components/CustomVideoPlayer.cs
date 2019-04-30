5447a2e0ab800eccd8dfeb37ddf724aad3752549
luoxuguang <xuguang.luo@unity3d.com>
2019-04-30 10:02:02 +0800

    player

--- Assets/ConnectApp/Components/CustomVideoPlayer.cs
+++ Assets/ConnectApp/Components/CustomVideoPlayer.cs
@@ -1,10 +1,15 @@
+using System;
 using System.Collections.Generic;
 using ConnectApp.constants;
 using ConnectApp.utils;
 using Unity.UIWidgets.foundation;
+using Unity.UIWidgets.material;
+using Unity.UIWidgets.rendering;
 using Unity.UIWidgets.widgets;
 using UnityEngine;
 using UnityEngine.Video;
+using Color = Unity.UIWidgets.ui.Color;
+using Icons = ConnectApp.constants.Icons;
 using RawImage = UnityEngine.UI.RawImage;
 using Texture = Unity.UIWidgets.widgets.Texture;
 
@@ -28,6 +33,7 @@ namespace ConnectApp.components {
     public class _CustomVideoPlayerState : State<CustomVideoPlayer> {
         private VideoPlayer _player = null;
         private RenderTexture _texture = null;
+        
         private RawImage image;
         private bool isPaused = false;
 
@@ -54,30 +60,31 @@ namespace ConnectApp.components {
                         left:0,
                         right:0,
                         child:new Container(
+                            height:44,
+                            color:Color.fromRGBO(0,0,0,0.1f),
                             child:new Row(
+                                mainAxisAlignment:MainAxisAlignment.spaceAround,
+                                crossAxisAlignment:CrossAxisAlignment.center,
                                 children:new List<Widget>
                                 {
-                                    new Text($"{DateConvert.formatTime(_player.frameCount / _player.frameRate)}")
+                                    new GestureDetector(
+                                        child:new Container(
+                                            color:CColors.Transparent,
+                                            child:new Icon(Icons.replay,size:20,color:CColors.White)
+                                        )
+                                    ),
+                                    new Text($"{DateConvert.formatTime((float)_player.time)}",style:CTextStyle.CaptionWhite),
+                                    
+                                    new Text($"{DateConvert.formatTime(_player.frameCount / _player.frameRate)}",style:CTextStyle.CaptionWhite),
+                                    new GestureDetector(
+                                        child:new Container(
+                                            color:CColors.Transparent,
+                                            child:new Icon(Icons.favorite,size:20,color:CColors.White)
+                                        )
+                                    )
                                 })
-                            )),
-                    Positioned.fill(
-                        child: isPaused
-                            ? new GestureDetector(
-                                onTap: () => {
-                                    _player.Play();
-                                    setState(() => { isPaused = false; });
-                                },
-                                child: new Icon(Icons.play_arrow, null, 64, CColors.White)
                             )
-                            : new GestureDetector(
-                                onTap: () => {
-                                    _player.Pause();
-                                    setState(() => { isPaused = true; });
-                                },
-                                child: new Container(
-                                    color: CColors.Transparent)
-                            )
-                    )
+                        )
                 })
             );
         }
@@ -90,30 +97,18 @@ namespace ConnectApp.components {
             player.audioOutputMode = VideoAudioOutputMode.AudioSource;
             player.targetTexture = _texture;
             player.isLooping = false;
+            player.playOnAwake = true;
             player.aspectRatio = VideoAspectRatio.FitOutside;
-            player.prepareCompleted += Prepared;
             player.sendFrameReadyEvents = true;
+            player.prepareCompleted += Prepared;
             player.Prepare();
             return player;
         }
 
         void Prepared(VideoPlayer player)
         {
-            
             player.Play();
         }
 
-//            ｛
-//        //把图像赋给RawImage
-//        image.texture = p.texture;
-//        //帧数/帧速率=总时长    如果是本地直接赋值的视频，我们可以通过VideoClip.length获取总时长
-//        sliderVideo.maxValue = vPlayer.frameCount / vPlayer.frameRate;
-//        time = sliderVideo.maxValue;
-//        hour = (int)time / 60;
-//        mint = (int)time % 60;
-//        text_Count.text = string.Format("/  ｛0:D2｝:｛1:D2｝", hour, mint);
-//        sliderVideo.onValueChanged.AddListener(delegate ｛ ChangeVideo(sliderVideo.value); ｝);
-//        player.Play();
-//        ｝
     }
 }
