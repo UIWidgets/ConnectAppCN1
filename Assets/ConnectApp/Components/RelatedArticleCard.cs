b712a45a53fec1213ba13ebfdf9c1e29751f018c
luoxuuguang <xuguang.luo@unity3d.com>
2019-07-19 15:09:25 +0800

    fix : fix article time fix : fix about us color

--- Assets/ConnectApp/Components/RelatedArticleCard.cs
+++ Assets/ConnectApp/Components/RelatedArticleCard.cs
@@ -39,7 +39,7 @@ namespace ConnectApp.Components {
             const float borderRadius = 4;
 
             var gap = this.topPadding ? 16 : 0;
-            var time = this.article.createdTime;
+            var time = this.article.publishedTime;
             var child = new Container(
                 color: CColors.White,
                 padding: EdgeInsets.only(16, 16 + gap, 16, 16),
