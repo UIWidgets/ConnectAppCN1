eec48b0ea401058629c02c34e47416e814e8baa3
wangshuang <wangshuang@unity3d.com>
2019-08-31 22:26:22 +0800

    update: update version 1.1.1 -> 1.1.2.

--- Assets/ConnectApp/Constants/Config.cs
+++ Assets/ConnectApp/Constants/Config.cs
@@ -16,9 +16,9 @@ namespace ConnectApp.Constants {
 
         public const string MINIID = "gh_f731aec0bdd0";
 
-        public const string versionNumber = "1.1.1";
+        public const string versionNumber = "1.1.2";
 
-        public const int versionCode = 53;
+        public const int versionCode = 54;
 
         public const string originCodeUrl = "https://github.com/UnityTech/ConnectAppCN";
 
