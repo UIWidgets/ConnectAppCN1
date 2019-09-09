236360b762e8455021e23e2dc19e7e9e91169db3
Yuncong Zhang <yuncong.zhang@unity3d.com>
2019-09-09 14:14:47 +0800

    Create Channel Screen.

---
+++ Assets/ConnectApp/Models/ViewModel/ChannelScreenViewModel.cs
@@ -0,0 +1,8 @@
+using System.Collections.Generic;
+using ConnectApp.Models.Model;
+
+namespace ConnectApp.Models.ViewModel {
+    public class ChannelScreenViewModel {
+        public List<Channel> discoverChannelInfo;
+    }
+}
---
+++ Assets/ConnectApp/Screens/ChannelScreen.cs
@@ -0,0 +1,145 @@
+using System.Collections.Generic;
+using System.Linq;
+using ConnectApp.Components;
+using ConnectApp.Constants;
+using ConnectApp.Models.ActionModel;
+using ConnectApp.Models.Model;
+using ConnectApp.Models.State;
+using ConnectApp.Models.ViewModel;
+using ConnectApp.redux.actions;
+using Unity.UIWidgets.foundation;
+using Unity.UIWidgets.painting;
+using Unity.UIWidgets.Redux;
+using Unity.UIWidgets.widgets;
+
+namespace ConnectApp.screens {
+    public class ChannelScreenConnector : StatelessWidget {
+        public ChannelScreenConnector(
+            Key key = null
+        ) : base(key: key) {
+        }
+
+        public override Widget build(BuildContext context) {
+            return new StoreConnector<AppState, ChannelScreenViewModel>(
+                converter: state => {
+                    return new ChannelScreenViewModel {
+                        discoverChannelInfo = new List<Channel> {
+                            new Channel {
+                                imageUrl =
+                                    "https://connect-prd-cdn.unity.com/20190830/p/images/9796aa86-b799-4fcc-a2df-ac6d1293ea8e_image1_1_1280x720.jpg",
+                                name = "Unite Shanghai 技术会场",
+                                members = new List<User>(),
+                                isHot = true,
+                                joined = true,
+                            },
+                            new Channel {
+                                imageUrl =
+                                    "https://connect-prd-cdn.unity.com/20190830/p/images/9796aa86-b799-4fcc-a2df-ac6d1293ea8e_image1_1_1280x720.jpg",
+                                name = "游戏开发日常吐槽",
+                                members = new List<User>(),
+                                isHot = true,
+                            },
+                            new Channel {
+                                imageUrl =
+                                    "https://connect-prd-cdn.unity.com/20190830/p/images/9796aa86-b799-4fcc-a2df-ac6d1293ea8e_image1_1_1280x720.jpg",
+                                name = "我们都爱玩游戏",
+                                members = new List<User>(),
+                                joined = true,
+                            },
+                            new Channel {
+                                imageUrl =
+                                    "https://connect-prd-cdn.unity.com/20190830/p/images/9796aa86-b799-4fcc-a2df-ac6d1293ea8e_image1_1_1280x720.jpg",
+                                name = "今天你学Unity了吗",
+                                members = new List<User>(),
+                            },
+                            new Channel {
+                                imageUrl =
+                                    "https://connect-prd-cdn.unity.com/20190830/p/images/9796aa86-b799-4fcc-a2df-ac6d1293ea8e_image1_1_1280x720.jpg",
+                                name = "Unity深圳Meetup",
+                                members = new List<User>(),
+                            }
+                        }
+                    };
+                },
+                builder: (context1, viewModel, dispatcher) => {
+                    var actionModel = new ChannelScreenActionModel {
+                        mainRouterPop = () => dispatcher.dispatch(new MainNavigatorPopAction()),
+                    };
+                    return new ChannelScreen(viewModel, actionModel);
+                }
+            );
+        }
+    }
+
+    public class ChannelScreen : StatefulWidget {
+        public ChannelScreen(
+            ChannelScreenViewModel viewModel = null,
+            ChannelScreenActionModel actionModel = null,
+            Key key = null
+        ) : base(key: key) {
+            this.viewModel = viewModel;
+            this.actionModel = actionModel;
+        }
+
+        public readonly ChannelScreenViewModel viewModel;
+        public readonly ChannelScreenActionModel actionModel;
+
+        public override State createState() {
+            return new _ChannelScreenState();
+        }
+    }
+
+    class _ChannelScreenState : State<ChannelScreen> {
+        TextEditingController _fullNameController;
+        TextEditingController _titleController;
+
+        readonly FocusNode _fullNameFocusNode = new FocusNode();
+        readonly FocusNode _titleFocusNode = new FocusNode();
+        Dictionary<string, string> _jobRole;
+
+        public override void initState() {
+            base.initState();
+        }
+
+        public override Widget build(BuildContext context) {
+            return new Container(
+                color: CColors.White,
+                child: new CustomSafeArea(
+                    bottom: false,
+                    child: new Container(
+                        color: CColors.Background,
+                        child: new Column(
+                            children: new List<Widget> {
+                                this._buildNavigationBar(),
+                                new Flexible(
+                                    child: this._buildContent()
+                                )
+                            }
+                        )
+                    )
+                )
+            );
+        }
+
+        Widget _buildNavigationBar() {
+            return new CustomAppBar(
+                onBack: () => this.widget.actionModel.mainRouterPop(),
+                new Text(
+                    "发现群聊",
+                    style: CTextStyle.PXLargeMedium
+                )
+            );
+        }
+
+        Widget _buildContent() {
+            return new Container(
+                color: CColors.White,
+                child: new ListView(
+                    padding: EdgeInsets.symmetric(16, 0),
+                    children: this.widget.viewModel.discoverChannelInfo
+                        .Select(MessageBuildUtils.buildDiscoverChannelItem).ToList()
+                )
+            );
+        }
+    }
+}
