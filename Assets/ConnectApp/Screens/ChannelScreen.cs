7d581f31f5349c41f04109377a113828303f6b53
Yuncong Zhang <yuncong.zhang@unity3d.com>
2019-09-24 15:56:08 +0800

    Fix compile error on ios.

--- Assets/ConnectApp/Screens/ChannelScreen.cs
+++ Assets/ConnectApp/Screens/ChannelScreen.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using ConnectApp.Components;
 using ConnectApp.Components.pull_to_refresh;
 using ConnectApp.Constants;
