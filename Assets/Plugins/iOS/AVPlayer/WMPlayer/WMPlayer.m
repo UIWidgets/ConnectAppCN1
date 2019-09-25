82151d5704bc8297d72a52ea26d72dbdd05761b4
luoxuuguang <xuguang.luo@unity3d.com>
2019-09-25 12:13:38 +0800

    fix : fix player bug

--- Assets/Plugins/iOS/AVPlayer/WMPlayer/WMPlayer.m
+++ Assets/Plugins/iOS/AVPlayer/WMPlayer/WMPlayer.m
@@ -949,9 +949,10 @@ static void *PlayViewStatusObservationContext = &PlayViewStatusObservationContex
                 }
             }
             if(!self.loopPlay){
-                self.state = WMPlayerStateFinished;
-                self.bottomProgress.progress = 0;
                 dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+                    [self.progressSlider setValue:0];
+                    self.state = WMPlayerStateFinished;
+                    self.bottomProgress.progress = 0;
                     [self pause];
                 });
             }
