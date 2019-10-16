6f25e947ffdc50a68e3945905836f67952cc2a17
Yuncong Zhang <yuncong.zhang@unity3d.com>
2019-10-16 11:37:58 +0800

    Save new notifications.

---
+++ Assets/ConnectApp/Utils/NewNotificationManager.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using ConnectApp.Models.Model;
+using Newtonsoft.Json;
+using Unity.UIWidgets.foundation;
+using UnityEngine;
+
+namespace ConnectApp.Utils {
+    public class NewNotificationManager {
+        const string _newNotifications = "NewNotifications";
+
+        public static void saveNewNotification(string userId, string newNotification) {
+            var dict = getNewNotifications();
+            if (dict.ContainsKey(userId) && newNotification == null) {
+                dict.Remove(userId);
+            }
+            else {
+                dict[userId] = newNotification;
+            }
+            var infoStr = JsonConvert.SerializeObject(dict);
+            PlayerPrefs.SetString(_newNotifications, infoStr);
+            PlayerPrefs.Save();
+        }
+
+        public static string getNewNotification(string userId) {
+            var dict = getNewNotifications();
+            return dict.ContainsKey(userId) ? dict[userId] : null;
+        }
+
+        public static Dictionary<string, string> getNewNotifications() {
+            if (!PlayerPrefs.HasKey(_newNotifications)) {
+                return new Dictionary<string, string>();
+            }
+
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(PlayerPrefs.GetString(_newNotifications));
+        }
+
+        public static void clearNewNotifications() {
+            if (PlayerPrefs.HasKey(_newNotifications)) {
+                PlayerPrefs.DeleteKey(_newNotifications);
+            }
+        }
+    }
+}
