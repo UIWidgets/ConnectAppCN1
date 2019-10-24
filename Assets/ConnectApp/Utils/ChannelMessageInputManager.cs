2686d5be7a44ed32ddaabc22901d3dbaadd1d4a9
Json <785123832@qq.com>
2019-10-24 11:57:38 +0800

    fixed

--- Assets/ConnectApp/Utils/ChannelMessageInputManager.cs
+++ Assets/ConnectApp/Utils/ChannelMessageInputManager.cs
@@ -7,12 +7,25 @@ using Unity.UIWidgets.foundation;
 namespace ConnectApp.Utils {
     static class ChannelMessageMentionHelper {
         public static string parseMention(this string text, Dictionary<string, string> replacements) {
+            if (text.isEmpty()) {
+                return "";
+            }
+
+            if (replacements.isNullOrEmpty()) {
+                return text;
+            }
+
             var regex = new Regex(String.Join("|",replacements.Keys.Select(k => "@" + Regex.Escape(k))));
             var replaced = regex.Replace(input: text, m => {
                 if (m.Value.isEmpty()) {
                     return "";
                 }
-                return "<@" + replacements[m.Value.Substring(1)] + ">";
+
+                if (replacements.ContainsKey(m.Value.Substring(1))) {
+                    return "<@" + replacements[m.Value.Substring(1)] + ">";
+                }
+
+                return "";
             });
             return replaced;
         }
