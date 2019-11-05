faddbbaec889dbab48e86c9984c7131d76ffd8d2
Json <785123832@qq.com>
2019-11-05 15:39:52 +0800

    fixed

--- Assets/ConnectApp/Redux/Reducers/Reducer.cs
+++ Assets/ConnectApp/Redux/Reducers/Reducer.cs
@@ -106,7 +106,7 @@ namespace ConnectApp.redux.reducers {
                     state.favoriteState.favoriteDetailArticleIdDict = new Dictionary<string, List<string>>();
                     state.channelState.clearMentions();
                     state.channelState.mentionSuggestions.Clear();
-                    state.channelState.channelDict = new Dictionary<string, ChannelView>();
+                    state.channelState.channelDict.Clear();
                     break;
                 }
 
