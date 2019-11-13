cc91d7605ad3714084833e85512f6ee8cc9dd895
luoxuuguang <xuguang.luo@unity3d.com>
2019-11-13 18:58:40 +0800

    fix Android push mode

---
+++ Assets/ConnectApp/Plugins/platform/android/SystemReceiver.java
@@ -0,0 +1,39 @@
+package com.unity3d.unityconnect;
+
+import android.app.Notification;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.media.AudioManager;
+
+import com.unity3d.unityconnect.plugins.CommonPlugin;
+
+import cn.jpush.android.api.BasicPushNotificationBuilder;
+import cn.jpush.android.api.JPushInterface;
+
+public class SystemReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        if (intent.getAction().equals(AudioManager.RINGER_MODE_CHANGED_ACTION)) {
+            BasicPushNotificationBuilder builder = new BasicPushNotificationBuilder(CommonPlugin.mContext);
+            AudioManager am = (AudioManager)CommonPlugin.mContext.getSystemService(Context.AUDIO_SERVICE);
+            final int ringerMode = am.getRingerMode();
+            switch (ringerMode) {
+                case AudioManager.RINGER_MODE_NORMAL:
+                    //normal
+                    break;
+                case AudioManager.RINGER_MODE_VIBRATE:
+                    //vibrate
+                    builder.notificationDefaults =  Notification.DEFAULT_VIBRATE
+                            | Notification.DEFAULT_LIGHTS; // 设置为铃声、震动、呼吸灯闪烁都要
+                    JPushInterface.setPushNotificationBuilder(1, builder);
+                    break;
+                case AudioManager.RINGER_MODE_SILENT:
+                    //silent
+                    builder.notificationDefaults = Notification.DEFAULT_LIGHTS; // 设置为铃声、震动、呼吸灯闪烁都要
+                    JPushInterface.setPushNotificationBuilder(1, builder);
+                    break;
+            }
+        }
+    }
+}
