20e6a869529820f939da1492dee6269dfc23823d
Yuncong Zhang <yuncong.zhang@unity3d.com>
2019-11-25 14:28:39 +0800

    Cleanup.

--- Assets/ConnectApp/Screens/EventsScreen.cs
+++ Assets/ConnectApp/Screens/EventsScreen.cs
@@ -4,7 +4,6 @@ using ConnectApp.Constants;
 using ConnectApp.Utils;
 using Unity.UIWidgets.painting;
 using Unity.UIWidgets.widgets;
-using UnityEngine;
 
 namespace ConnectApp.screens {
     public class EventsScreen : StatefulWidget {
@@ -36,7 +35,7 @@ namespace ConnectApp.screens {
                                     new EventCompletedScreenConnector()
                                 },
                                 newValue => AnalyticsManager.ClickEventSegment("Event",
-                                        0 == newValue ? "ongoing" : "completed"),
+                                    0 == newValue ? "ongoing" : "completed"),
                                 1
                             )
                         )
