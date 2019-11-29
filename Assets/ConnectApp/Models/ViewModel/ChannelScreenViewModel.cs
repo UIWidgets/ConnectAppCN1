dc34ee09b1d04b7e204a5fc0baeb31549635d61c
xingwei.zhu <etzhu192@hotmail.com>
2019-11-29 16:56:00 +0800

    new mention features => show loading + mention dynamic rank

--- Assets/ConnectApp/Models/ViewModel/ChannelScreenViewModel.cs
+++ Assets/ConnectApp/Models/ViewModel/ChannelScreenViewModel.cs
@@ -15,7 +15,7 @@ namespace ConnectApp.Models.ViewModel {
         public bool mentionAutoFocus;
         public string mentionUserId;
         public string mentionUserName;
-        public Dictionary<string, ChannelMember> mentionSuggestion;
+        public List<ChannelMember> mentionSuggestion;
         public bool hasChannel;
         public bool channelError;
         public ChannelMessageView waitingMessage;
