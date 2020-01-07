4822748792aba9836a1c4fb357c99afb0c72c3c0
wangshuang <wangshuang@unity3d.com>
2020-01-07 12:15:00 +0800

    update: remove redundant code.

--- Assets/ConnectApp/Utils/CStringUtils.cs
+++ Assets/ConnectApp/Utils/CStringUtils.cs
@@ -88,18 +88,6 @@ namespace ConnectApp.Utils {
             return $"{bytes / (1024.0f * 1024 * 1024):F}G";
         }
 
-        public static string NotificationText(int num) {
-            if (num == 0) {
-                return null;
-            }
-
-            if (num < 100) {
-                return $"{num}";
-            }
-
-            return "";
-        }
-
         public static long hexToLong(this string number, long defaultValue = -1) {
             if (number.isEmpty()) {
                 return defaultValue;
